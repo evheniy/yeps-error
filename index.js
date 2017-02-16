39662d5a99c0f11cd20d80795a922982b504f6ea
Evheniy Bystrov <evheniy.bystrov@just-eat.com>
2017-02-16 11:57:16 +0000

    500 error done

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ const debug = require('debug')('yeps:error');
 module.exports = () => async context => {
     debug('Add error handler');
 
-    context.app.catch(ctx => {
+    context.app.catch(async (err, ctx) => {
         debug('Register 500 error handler');
-        if (!ctx.req.finished) {
+        debug(err);
+        if (!ctx.res.finished) {
             debug('Send 500 error handler');
             ctx.res.writeHead(500);
             ctx.res.end('Internal Server Error');
         }
     });
 
-    context.app.resolve = () => {
-        debug('Default resolve');
+    /* context.app.resolve = () => {
+        debug('Update resolve');
         context.app.then(ctx => {
             debug('Register 404 error handler');
-            if (!ctx.req.finished) {
+            if (!ctx.res.finished) {
                 debug('Send 404 error handler');
                 ctx.res.writeHead(404);
                 ctx.res.end('Not Found');
             }
         });
+        debug('Old resolve');
         return context.app.resolve();
-    };
+    };*/
 };
--- tests/index.js
+++ tests/index.js
@@ -2,7 +2,7 @@ const App = require('yeps');
 const error = require('..');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-// const http = require('http');
+const http = require('http');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
@@ -14,25 +14,43 @@ describe('YAPS error handler test', () => {
         app = new App();
     });
 
-    it('should test 404', async () => {
+    it('should test 404'/* , async () => {
+
+     let isTestFinished = false;
+
+     app.then(error());
+
+     await chai.request(http.createServer(app.resolve()))
+     .get('/')
+     .send()
+     .catch(err => {
+     expect(err).to.have.status(404);
+     expect(err.message).to.be.equal('Not Found');
+     isTestFinished = true;
+     });
+
+     expect(isTestFinished).is.true;
+     }*/);
+
+    it('should test 500', async() => {
 
         let isTestFinished = false;
 
         app.then(error());
+        app.then(async() => {
+            throw new Error('test');
+        });
 
-        /* await chai.request(http.createServer(app.resolve()))
+        await chai.request(http.createServer(app.resolve()))
             .get('/')
             .send()
             .catch(err => {
-                console.log(err);
-                expect(err).to.have.status(404);
-                expect(err.message).to.be.equal('Not Found');
+                expect(err).to.have.status(500);
+                expect(err.message).to.be.equal('Internal Server Error');
                 isTestFinished = true;
-            }); */
-        isTestFinished = true;
+            });
+
         expect(isTestFinished).is.true;
     });
-
-    it('should test 500');
 
 });
